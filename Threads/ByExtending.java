1339c09eab54e0b0589d03a98aa4fa48afbe8583
kauramanp <33446281+kauramanp@users.noreply.github.com>
2023-07-31 23:52:31 +0530

    added program

---
+++ Threads/ByExtending.java
@@ -0,0 +1,28 @@
+class ByExtending extends Thread {
+    int a = 0;
+
+    ByExtending(int a) {
+        this.a = a;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++)
+            System.out.println("in run method " + a);
+    }
+
+    public static void main(String args[]) {
+        ByExtending byExtending = new ByExtending(0);
+        byExtending.start();
+
+        ByExtending byExtending1 = new ByExtending(1);
+        try {
+            byExtending1.start();
+
+            byExtending.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
